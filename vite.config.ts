6d4ca820ca710bcf2d86fb51c06dc2fa39a8bc31
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2025-05-05 16:58:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",  // This makes the app accessible externally
+    port: process.env.PORT || 4000,  // Render automatically sets the PORT env variable
+  },
   plugins: [react()],
   base: "/",
   optimizeDeps: {
